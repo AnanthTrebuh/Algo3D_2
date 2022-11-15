de3f078c27def06405cedbda81c1be9492242cb2
Tiwan17 <lenglet.jolan17@hotmail.com>
2022-11-15 08:33:32 +0100

    Fix fresnel bug

--- obj.fs
+++ obj.fs
@@ -14,32 +14,28 @@ uniform float uRefracValue;
 // ==============================================
 void main(void)
 {
-
-	//i reflect
-	//m normal
-	//n indice
-	// R 
-	// T = 1 - R
-
 	float ratio = 1.0/ uRefracValue;
 	vec3 I = normalize(pos3D.xyz);
-	vec3 Re = reflect(I, normalize(N));
-	Re = (rMat * vec4(Re, 1.0)).xyz;
-	vec4 textRe = vec4(textureCube(uSkybox, normalize(Re.xzy)).rgb, 1.0);
-	vec3 Ra = refract(I, normalize(N), ratio);
-	Ra = (rMat * vec4(Ra, 1.0)).xyz;
-	vec4 textRa = vec4(textureCube(uSkybox, normalize(Ra.xzy)).rgb, 1.0);
-	// vec3 R = Re + Ra;
+	vec3 Reflection = reflect(I, normalize(N));
+	Reflection = (rMat * vec4(Reflection, 1.0)).xyz;
+	vec4 textRe = vec4(0,0,0,0);
+	textRe = textureCube(uSkybox, Reflection.xzy);
+	vec3 Refraction = refract(I, normalize(N), ratio);
+	Refraction = (rMat * vec4(Refraction, 1.0)).xyz;
+	vec4 textRa = vec4(0,0,0,0);
+	textRa = textureCube(uSkybox, Refraction.xzy);
 
-	float c = abs(dot(Ra, normalize(N)));
+	float c = abs(dot(I, normalize(N)));
 	float g = sqrt((uRefracValue*uRefracValue) + ( c*c) - 1.0);
-    float R = 0.5 * (((g-c)*(g-c))/((g+c)*(g+c))) * ( 1.0 + ((c * (g+c)-1.0) * (c * (g+c)-1.0)) / ((c * (g-c)+1.0) * (c * (g-c)+1.0) ));
+    float R = 0.5 * 
+			(((g-c)*(g-c))/((g+c)*(g+c))) * 
+			( 1.0 + (((c * (g+c)-1.0) * (c * (g+c)-1.0)) / ((c * (g-c)+1.0) * (c * (g-c)+1.0) )));
 	float T = 1.0 - R;
 
 	if(uIsMirror && uIsRefrac){
 		textRa = textRa * R;
 		textRe = textRe * T;
-		gl_FragColor = textRa  + textRe;
+		gl_FragColor = textRa  + textRe;    
 	}
 	else if (uIsMirror){
 		gl_FragColor = textRe;
@@ -51,28 +47,6 @@ void main(void)
 		vec3 col = vec3(0.0,0.6,0.4) * dot(N,normalize(vec3(-pos3D))); // Lambert rendering, eye light source
 		gl_FragColor = vec4(col,1.0);
 	}
-	
-	
-
-	// if(uIsMirror){
-	// 	vec3 I = normalize(pos3D).xyz;
-	// 	vec3 R = reflect(I, normalize(N));
-	// 	R = vec3(rMat * vec4(R, 1.0));
-	// 	gl_FragColor = vec4(textureCube(uSkybox, vec3(R.x, R.z, R.y)).rgb, 1.0);
-	// }
-	// else if (uIsRefrac)
-	// {
-	// 	float ratio = 0.7;
-	// 	vec3 I = normalize(pos3D).xyz;
-	// 	vec3 R = refract(I, normalize(N), ratio);
-	// 	R = vec3(rMat * vec4(R, 1.0));
-	// 	gl_FragColor = vec4(textureCube(uSkybox, vec3(R.x, R.z, R.y)).rgb, 1.0);
-	// }
-	// else 
-	// {
-	// 	vec3 col = vec3(0.0,0.6,0.4) * dot(N,normalize(vec3(-pos3D))); // Lambert rendering, eye light source
-	// 	gl_FragColor = vec4(col,1.0);
-	// }
 }
 
 
