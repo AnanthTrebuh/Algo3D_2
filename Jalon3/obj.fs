c78bdde474f186e54b95694da580df87b82fcb51
Tiwan17 <lenglet.jolan17@hotmail.com>
2023-02-08 15:10:02 +0100

    update

--- Jalon3/obj.fs
+++ Jalon3/obj.fs
@@ -68,13 +68,27 @@ float rand(vec2 co){
 
 void main(void)
 {
+	float pi = 3.14159265359;
 	float ratio = 1.0/ uRefracValue;
 	vec3 I = normalize(pos3D.xyz);
 
-	vec4 textRefrac = uIsRefrac ? refraction(I, N, ratio) : vec4(0,0,0,0);
-	vec4 textMirror = uIsMirror ? reflection(I, N) : vec4(0,0,0,0);
+	vec4 textRefrac;
+	vec4 textMirror;
 
-	
+	// calcul de la refraction
+	if(uIsRefrac){
+		textRefrac = refraction(I, N, ratio);
+	}else{
+		textRefrac = vec4(0,0,0,0);
+	}
+
+	// calcul de la reflection
+	if(uIsMirror){
+		textMirror = reflection(I, N);
+	}else{
+		textMirror = vec4(0,0,0,0);
+	}
+
 	if(uIsMirror && uIsRefrac){
 		float R = fresnel(I, N, uRefracValue);
 		float T = 1.0 - R;
@@ -92,10 +106,8 @@ void main(void)
 		vec3 o = normalize(-pos3D.xyz);
 		vec3 M = normalize(i + o);
 
-		float cosT = dot(normalize(N),normalize(M));
-		float pi = 3.14159265359;
-
 		float NdotM = dot(normalize(N),normalize(M));
+		float cosT = NdotM;
 		float NdotI = dot(normalize(N),normalize(i));
 		float NdotO = dot(normalize(N),normalize(o));
 		float OdotM = dot(normalize(o),normalize(M));
@@ -110,22 +122,75 @@ void main(void)
 		vec3 Fr = diffuse + speculaire;
 		vec3 L = (2.0) * Fr * NdotI;
 
-		if (uIsSample){
+		// if (uIsSample){
 
-			// float ksi1 = rand(gl_FragCoord.xy);
-			// float ksi2 = rand(gl_FragCoord.xy + vec2(1.0,1.0));
+		// 	float ksi1 = rand(gl_FragCoord.xy);
+		// 	float ksi2 = rand(gl_FragCoord.xy + ksi1);
 
-			// float phi = 2.0 * pi * ksi1;
-			// float theta = atan(sqrt(-uSigmaValue * log(1.0-ksi2)));
+		// 	float phi = 2.0 * pi * ksi2;
+		// 	float theta = atan(sqrt(-uSigmaValue * log(1.0-ksi1)));
 
-			// float x = sin(theta) * cos(phi);
-			// float y = sin(theta) * sin(phi);
-			// float z = cos(theta);
+		// 	float x = sin(theta) * cos(phi);
+		// 	float y = sin(theta) * sin(phi);
+		// 	float z = cos(theta);
 
-			// vec3 H = normalize(vec3(x,y,z));
-		}
+		// 	vec3 m = normalize(vec3(x,y,z));
+		// 	// rotation de m 
+
+		// 	float pdf = D * dot(normalize(m), normalize(N));			
+
+ 		// 	// vec3 iN = vec3(1,0,0);
+		// 	// if(dot(normalize(iN), normalize(i)) > 0.9){
+		// 	// 	iN = vec3(0,1,0);
+		// 	// }
+		// 	// vec3 jN = normalize(cross(iN, N));
+		// 	// iN = normalize(cross(jN, N));
+
+		// 	// mat3 rot_1 = mat3(iN, jN, N);
+		// 	// mat3 rot = rot_1 * vec3(1.0,0.0,0.0);
+		// 	// mat3 rot = toMat3(multiplyVec3(toMat4(rot_1), vec3(1.0,0.0,0.0)));
+
+		// 	//vec4 iEchan = reflection(o, m);
+
+		// 	gl_FragColor = vec4(0.0,m.y,0.0,1.0);
+		// }
 
 		gl_FragColor = vec4(L,1.0);
+	}
+	else if (uIsSample){
+		float ksi1 = rand(gl_FragCoord.xy);
+		float ksi2 = rand(gl_FragCoord.xy + ksi1);
+
+		float phi = 2.0 * pi * ksi2;
+		float theta = atan(sqrt(-uSigmaValue * log(1.0-ksi1)));
+
+		float x = sin(theta) * cos(phi);
+		float y = sin(theta) * sin(phi);
+		float z = cos(theta);
+
+		vec3 m = normalize(vec3(x,y,z));
+		// rotation de m 
+
+		float NdotM = dot(normalize(N),normalize(m));
+		float cosT = NdotM;
+		float D = beckmann(cosT, uSigmaValue, pi);
+
+		float pdf = D * dot(normalize(m), normalize(N));			
+
+		// vec3 iN = vec3(1,0,0);
+		// if(dot(normalize(iN), normalize(i)) > 0.9){
+		// 	iN = vec3(0,1,0);
+		// }
+		// vec3 jN = normalize(cross(iN, N));
+		// iN = normalize(cross(jN, N));
+
+		// mat3 rot_1 = mat3(iN, jN, N);
+		// mat3 rot = rot_1 * vec3(1.0,0.0,0.0);
+		// mat3 rot = toMat3(multiplyVec3(toMat4(rot_1), vec3(1.0,0.0,0.0)));
+
+		//vec4 iEchan = reflection(o, m);
+
+		gl_FragColor = vec4(0.0,m.y,0.0,1.0);
 	}
 	else {
 		vec3 col = uColorObj * dot(N,normalize(vec3(-pos3D))); // Lambert rendering, eye light source
